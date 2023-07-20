0047c1c682b1f5282d7f386f0f4d5379e4e1b3d7
Leonardoascencion <139736278+Leonardoascencion@users.noreply.github.com>
2023-07-20 16:30:13 -0400

    Add files via upload
    proving the repository

---
+++ proyecto/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+using System.IO;
+using System;
+using System.Collections.Generic;
+int i = 0;
+
+class Program
+{
+static void hacer_la_matriz()
+{
+string entrada = Console.ReadLine();
+string[] queri = entrada.Split("");
+foreach (string item in queri)
+{
+queri[i].ToLower();
+queri [i] = Regex.Replace(queri[i],@"[^\w\s]",string.Empty);
+i++;
+}
+Dictionary<string,Dictionary<string,int>> queriTF = new Dictionary<string,Dictionary<string,int>>();
+foreach (string item in queri)
+{
+if(queriTF.ContainsKey(item)) queriTF[item][item]++;
+else queriTF[item][item] = 1;
+}
+}
+}
+
